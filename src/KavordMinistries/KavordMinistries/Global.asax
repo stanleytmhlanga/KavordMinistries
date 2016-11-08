f6345c79ccec5be2293b09c6ec32c29cabb7d0c0
Stanley Mhlanga <mhlangastnp@gmail.com>
2016-11-08 20:52:46 +0200

    Add Kavord project to git

---
+++ src/KavordMinistries/KavordMinistries/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KavordMinistries.MvcApplication" Language="C#" %>
---
+++ src/KavordMinistries/KavordMinistries/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KavordMinistries.Startup))]
+namespace KavordMinistries
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
